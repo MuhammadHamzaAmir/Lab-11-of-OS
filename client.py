60b543dadc9b736760df424e3994cedb7a661b04
Hamza <muhammadhamza2084@gmail.com>
2021-01-05 00:43:17 +0500

    Completed the file

--- client.py
+++ client.py
@@ -2,12 +2,13 @@ from socket import *
 import time
 
 def mainMenu():
-
+    delay_t = 0.5
     serverIP = input("Enter the IP address: ")
     username = input("Enter the user name: ")
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverIP, 95))
     clientSocket.send(username.encode('utf-8'))
+    main_file = ""
     
     while (True):
         print("**File Management System**")
@@ -32,57 +33,104 @@ def mainMenu():
 
         if choice == "1":
             fname = input(
-                "Enter the name for the text file (without extension): ")
+                "Enter the name for the text file: ")
             send_data = "Create#"+fname
+            main_file = fname
             clientSocket.send(send_data.encode('utf-8'))
-            time.sleep(0.5)
+            time.sleep(delay_t)
             output = clientSocket.recv(204800).decode('utf-8')
             print(output)
         elif choice == "2":
             fname = input(
-                "Enter the name for the text file you want to delete(without extension): ")
+                "Enter the name for the text file you want to delete: ")
             send_data = "Delete#"+fname
             clientSocket.send(send_data.encode('utf-8'))
-            time.sleep(0.5)
+            time.sleep(delay_t)
             output = clientSocket.recv(204800).decode('utf-8')
             print(output)
         elif choice == "3":
             fname = input(
-                "Enter the name of the file you want to open(without extension): ")
+                "Enter the name of the file you want to open: ")
             mode = input("Enter the file mode (r,w,a)")
-            ob.Open(fname, mode)
+            main_file = fname
+            send_data = "Open#"+fname+"#"+mode
+            clientSocket.send(send_data.encode('utf-8'))
+            time.sleep(delay_t)
+            output = clientSocket.recv(204800).decode('utf-8')
+            print(output)
         elif choice == "4":
             fname = input(
-                "Enter the name of the file you want to close(without extension): ")
-            ob.Close(fname)
+                "Enter the name of the file you want to close: ")
+            send_data = "Close#"+fname
+            clientSocket.send(send_data.encode('utf-8'))
+            time.sleep(delay_t)
+            output = clientSocket.recv(204800).decode('utf-8')
+            print(output)
         elif choice == "5":
-            ob.Read_From_File()
+            send_data = "read_from_file#"+main_file
+            clientSocket.send(send_data.encode('utf-8'))
+            time.sleep(delay_t)
+            output = clientSocket.recv(204800).decode('utf-8')
+            print(output)
         elif choice == "6":
             text = input("Enter data: ")
-            ob.write_to_file(text)
+            send_data = "write_to_file#"+main_file+"#"+text
+            clientSocket.send(send_data.encode('utf-8'))
+            time.sleep(delay_t)
+            output = clientSocket.recv(204800).decode('utf-8')
+            print(output)
         elif choice == "7":
             text = input("Enter data: ")
-            ob.appendFile(text)
+            send_data = "appendFile#"+main_file+"#"+text
+            clientSocket.send(send_data.encode('utf-8'))
+            time.sleep(delay_t)
+            output = clientSocket.recv(204800).decode('utf-8')
+            print(output)
         elif choice == "8":
             start = input("Enter the starting point: ")
             size = input("Enter the string you want to read till: ")
-            ob.read_from_file(start, size)
+            send_data = "read_from_file_at#"+main_file+"#"+start+"#"+size
+            clientSocket.send(send_data.encode('utf-8'))
+            time.sleep(delay_t)
+            output = clientSocket.recv(204800).decode('utf-8')
+            print(output)
         elif choice == "9":
             write_at = input("Enter the point where you want to write: ")
             text = input("Enter Data: ")
-            ob.Write_to_File(write_at, text)
+            cond = input("Do you want to overwrite: IF YES THEN WRITE 1 else 0")
+            send_data = "Write_to_File_over#"+main_file+"#"+write_at+"#"+text+"#"+cond
+            clientSocket.send(send_data.encode('utf-8'))
+            time.sleep(delay_t)
+            output = clientSocket.recv(204800).decode('utf-8')
+            print(output)
         elif choice == "10":
             maxsize = input("Enter the size of the file, you want: ")
-            ob.truncate(maxsize)
+            send_data = "truncate#"+main_file+"#"+maxsize
+            clientSocket.send(send_data.encode('utf-8'))
+            time.sleep(delay_t)
+            output = clientSocket.recv(204800).decode('utf-8')
+            print(output)
         elif choice == "11":
             start = input("Enter starting index: ")
             size = input("Enter the size of string: ")
             target = input("Enter where you want to put the string: ")
-            ob.Move_within_file(start, size, target)
+            send_data = "Move_within_file#"+main_file+"#"+start+"#"+size+"#"+target
+            clientSocket.send(send_data.encode('utf-8'))
+            time.sleep(delay_t)
+            output = clientSocket.recv(204800).decode('utf-8')
+            print(output)
         elif choice == "12":
-            ob.show_memory_map()
+            send_data = "show_memory_map"
+            clientSocket.send(send_data.encode('utf-8'))
+            time.sleep(delay_t)
+            output = clientSocket.recv(204800).decode('utf-8')
+            print(output)
         elif choice == "13":
-            ob.system_exit()
+            send_data = "MHA_ARA"
+            clientSocket.send(send_data.encode('utf-8'))
+            time.sleep(delay_t)
+            output = clientSocket.recv(204800).decode('utf-8')
+            print(output)
             break
         elif choice != "":
             print("\n Not Valid Choice Try again")
